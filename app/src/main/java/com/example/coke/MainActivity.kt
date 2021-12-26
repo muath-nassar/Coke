a6c9b6217285d5dabaa704bc78dbc031858fcd97
Muath Nassar <moadnassar@gmail.com>
2021-12-26 14:24:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/coke/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.coke
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.coke.databinding.ActivityMainBinding
+import com.example.coke.ui.BlankFragment
+
+class MainActivity : AppCompatActivity() {
+    lateinit var biniding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        biniding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(biniding.root)
+        biniding.bottomNavView.background = null
+        biniding.bottomNavView.backgroundTintList = null
+        supportFragmentManager.beginTransaction().add(R.id.fragmentContainer,BlankFragment()).commit()
+
+        biniding.fab.imageTintList = null
+        biniding.bottomNavView.itemIconTintList = null
+    }
+}
